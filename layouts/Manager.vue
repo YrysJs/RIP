6317ba2ebd692be8155557b6e83a750c39f8229d
Bekarys Atabayev <bekars97@gmail.com>
2025-04-13 14:21:37 +0500

    manager booking list

---
+++ layouts/Manager.vue
@@ -0,0 +1,18 @@
+<script setup>
+import LayoutTop from '~/components/layout/LayoutTop.vue'
+import ManagerSidebar from '~/components/layout/ManagerSidebar.vue'
+</script>
+
+<template>
+  <section class="supplier bg-[#FAFAFA]">
+    <LayoutTop />
+    <main class="supplier-content max-w-[1175px] mx-auto w-full flex gap-[20px] min-h-[100vh] my-[20px] rounded-[12px]">
+      <ManagerSidebar />
+      <div class="w-full">
+        <slot/>
+      </div>
+    </main>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
