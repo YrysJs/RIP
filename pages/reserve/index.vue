924b1a76a8376f02f24c07672c6662cfaf1ef7cf
YrysJpeg <iriskeldicloud@gmail.com>
2025-04-29 16:53:43 +0500

    fix: minor fix in reserve

--- pages/reserve/index.vue
+++ pages/reserve/index.vue
@@ -98,7 +98,73 @@ const pickCity = (item) => {
             <div class="w-full h-[60vh] rounded-xl overflow-hidden">
                 <img class="w-full h-[60vh] object-cover" src="/fake-map.png" alt="">
             </div>
-            <div>map-bottom</div>
+            <div class="bg-[#FFF] p-[24px] mt-[24px] rounded-lg">
+                <div class="flex justify-between items-center">
+                    <h3 class="text-2xl font-medium font-roboto text-[#222222]">Северное кладбище</h3>
+                    <div class="flex gap-[12px] items-center">
+                        <div class="flex gap-[8px] items-center">
+                            <div class="w-[36px] h-[16px] rounded-sm bg-[#43DC4966] border-2 border-[#43DC49]"></div>
+                            <p class="font-roboto text-sm">Сводобные места</p>
+                        </div>
+                        <div class="flex gap-[8px] items-center">
+                            <div class="w-[36px] h-[16px] rounded-sm bg-[#DCBA4366] border-2 border-[#DCBA43]"></div>
+                            <p class="font-roboto text-sm">Сводобные места</p>
+                        </div>
+                        <div class="flex gap-[8px] items-center">
+                            <div class="w-[36px] h-[16px] rounded-sm bg-[#93939366] border-2 border-[#939393]"></div>
+                            <p class="font-roboto text-sm">Сводобные места</p>
+                        </div>
+                    </div>
+                </div>
+                <p class="text-[#939393] font-roboto text-sm mt-[4px] mb-[8px]">Мусульманское кладбище</p>
+                <div class="flex gap-[24px] items-center text-[#222222]">
+                    <div class="flex gap-[8px] items-center">
+                        <img src="/icons/geo-icon.svg" alt=""> <span class="text-sm font-roboto">Улица Бейсебаева, 148, Алматы (4,6 км от вас)</span>
+                    </div>
+                    <div class="flex gap-[8px] items-center">
+                        <img src="/icons/phone.svg" alt=""> <span class="text-sm font-roboto">+7 777 777 77 77</span>
+                    </div>
+                </div>
+                <div class="flex gap-[24px] mt-[16px] mb-[32px]">
+                    <span class="text-base font-medium font-roboto">Вместимость: 55 000</span>
+                    <span class="text-base font-medium font-roboto">Cвободных мест: 29 750</span>
+                </div>
+                <p class="text-base font-roboto text-[#222222]">
+                    Крупнейшее кладбище города с общей площадью 20 га. На территории расположены мемориальные зоны, посвященные жертвам войны, и семейные участки. Здесь похоронены несколько известных деятелей культуры и науки.Услуги: организация похорон, аренда мест, благоустройство.
+                </p>
+            </div>
+            <div class="bg-[#FFF] p-[24px] mt-[24px] rounded-lg">
+                <div class="flex justify-between items-center">
+                    <h3 class="text-2xl font-medium font-roboto text-[#222222]">Северное кладбище</h3>
+                    <div class="flex gap-[34px] items-center">
+                        <button class="flex items-center gap-[8px] text-base font-medium font-roboto text-[#222222]">
+                            <img src="/icons/share.svg" alt=""> Отправить
+                        </button>
+                        <button class="w-[90px] h-[50px] border-2 border-[#224C4F] rounded-lg bg-[#fff] text-[#224C4F] font-roboto text-base font-medium">
+                            Отмена
+                        </button>
+                        <button class="w-[257px] h-[50px] border-2 border-[#224C4F] rounded-lg bg-[#224C4F] text-[#fff] font-roboto text-base font-medium">
+                            Объеденить и забронировать 
+                        </button>
+                    </div>
+                </div>
+                <p class="text-[#939393] font-roboto text-sm mt-[4px] mb-[8px]">Мусульманское кладбище</p>
+                <div class="flex gap-[24px] items-center text-[#222222]">
+                    <div class="flex gap-[8px] items-center">
+                        <img src="/icons/geo-icon.svg" alt=""> <span class="text-sm font-roboto">Улица Бейсебаева, 148, Алматы (4,6 км от вас)</span>
+                    </div>
+                    <div class="flex gap-[8px] items-center">
+                        <img src="/icons/phone.svg" alt=""> <span class="text-sm font-roboto">+7 777 777 77 77</span>
+                    </div>
+                </div>
+                <div class="flex gap-[24px] mt-[16px] mb-[32px]">
+                    <span class="text-base font-medium font-roboto">Вместимость: 55 000</span>
+                    <span class="text-base font-medium font-roboto">Cвободных мест: 29 750</span>
+                </div>
+                <p class="text-base font-roboto text-[#222222]">
+                    Крупнейшее кладбище города с общей площадью 20 га. На территории расположены мемориальные зоны, посвященные жертвам войны, и семейные участки. Здесь похоронены несколько известных деятелей культуры и науки.Услуги: организация похорон, аренда мест, благоустройство.
+                </p>
+            </div>
         </div>
     </div>
 </template>
