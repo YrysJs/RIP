98dd2958b57e49e8cbbb380b79523d30a7a026d4
admin-asb <asb210297@gmail.com>
2025-09-19 20:32:39 +0500

    мемориалы

--- pages/client/tickets/active/index.vue
+++ pages/client/tickets/active/index.vue
@@ -189,7 +189,7 @@ onMounted(async () => {
           </div>
         </div>
         <NuxtLink
-          class="block py-[15px] px-5 rounded-lg bg-[#E9B949] text-black text-sm font-medium mt-[16px] max-sm:w-full"
+          class="block w-fit py-[15px] px-5 rounded-lg bg-[#E9B949] text-black text-sm font-medium mt-[16px] max-sm:w-full text-center"
           :to="
             localePath({
               name: 'client-tickets-active-id',
