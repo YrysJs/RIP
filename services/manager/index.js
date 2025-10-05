42496bb03b026892d3e39077d06c1f6e6916c23a
Bekarys Atabayev <bekars97@gmail.com>
2025-10-06 04:07:14 +0500

    sad

--- services/manager/index.js
+++ services/manager/index.js
@@ -4,7 +4,7 @@ function getBurialRequests(params) {
     const { $axios } = useNuxtApp()
     return $axios({
         method: 'GET',
-        url: 'http://194.32.140.103:8094/api/v1/burial-requests/my',
+        url: useRuntimeConfig().public.apiBaseUrl + '/api/v8/burial-requests/my',
         params,
     })
 }
@@ -13,7 +13,7 @@ function getBurialRequestById(id) {
     const { $axios } = useNuxtApp()
     return $axios({
         method: 'GET',
-        url: `http://194.32.140.103:8094/api/v1/burial-requests/${id}`,
+        url: useRuntimeConfig().public.apiBaseUrl + `/api/v8/burial-requests/${id}`,
     })
 }
 
@@ -21,7 +21,7 @@ function getBurialRequestStatus(data) {
     const { $axios } = useNuxtApp()
     return $axios({
         method: 'PATCH',
-        url: `http://194.32.140.103:8094/api/v1/burial-requests/${data.id}/status`,
+        url: useRuntimeConfig().public.apiBaseUrl + `/api/v8/burial-requests/${data.id}/status`,
         data: {
             status: data.status,
             comment: data.comment,
