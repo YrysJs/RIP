dbc8e4678f88a137b57c9045a956be22d5f360ba
Bekarys Atabayev <bekars97@gmail.com>
2025-10-06 15:53:04 +0500

    asdasd

--- services/manager/index.js
+++ services/manager/index.js
@@ -4,7 +4,7 @@ function getBurialRequests(params) {
     const { $axios } = useNuxtApp()
     return $axios({
         method: 'GET',
-        url: useRuntimeConfig().public.apiBaseUrl + '/api/v8/burial-requests/my',
+        url: 'http://194.32.140.103:8094/api/v1/burial-requests/my',
         params,
     })
 }
@@ -13,7 +13,7 @@ function getBurialRequestById(id) {
     const { $axios } = useNuxtApp()
     return $axios({
         method: 'GET',
-        url: useRuntimeConfig().public.apiBaseUrl + `/api/v8/burial-requests/${id}`,
+        url: `http://194.32.140.103:8094/api/v1/burial-requests/${id}`,
     })
 }
 
@@ -21,7 +21,7 @@ function getBurialRequestStatus(data) {
     const { $axios } = useNuxtApp()
     return $axios({
         method: 'PATCH',
-        url: useRuntimeConfig().public.apiBaseUrl + `/api/v8/burial-requests/${data.id}/status`,
+        url: `http://194.32.140.103:8094/api/v1/burial-requests/${data.id}/status`,
         data: {
             status: data.status,
             comment: data.comment,
@@ -33,13 +33,12 @@ function burialRequestComplete(id) {
     const { $axios } = useNuxtApp()
     return $axios({
         method: 'PUT',
-        url: useRuntimeConfig().public.apiBaseUrl + `/api/v8/burial-requests/${id}/complete`,
+        url: `http://194.32.140.103:8094/api/v1/burial-requests/${id}/complete`,
         data: {
             "is_complete": true
         },
     })
 }
-
 
 export {
     getBurialRequests,
